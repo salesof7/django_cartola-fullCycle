20c4764d9807ea2f6e1f4d74a6d11f29f1e92c60
salesof7 <jpsaless2002@gmail.com>
2022-12-11 19:40:19 -0300

    feat: create player model

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    initial_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
