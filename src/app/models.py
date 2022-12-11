f70b8f78433033dcf66fd7cbc094a5cf85f5df18
salesof7 <jpsaless2002@gmail.com>
2022-12-11 19:45:10 -0300

    feat: create team model

--- src/app/models.py
+++ src/app/models.py
@@ -7,3 +7,10 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
