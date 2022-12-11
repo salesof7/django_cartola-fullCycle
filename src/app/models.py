a9ca487caefd92abf5df93dafed13e5c3f6b3e2b
salesof7 <jpsaless2002@gmail.com>
2022-12-11 20:09:09 -0300

    feat: create actions model for players

--- src/app/models.py
+++ src/app/models.py
@@ -42,3 +42,22 @@ class Match(models.Model):
 
     def __str__(self):
         return f"{self.team_a.name} x {self.team_b.name}"
+
+
+class Action(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.PROTECT)
+    team = models.ForeignKey(Team, on_delete=models.PROTECT)
+    player = models.ForeignKey(Player, on_delete=models.PROTECT)
+
+    minutes = models.IntegerField()
+
+    class Actions(models.TextChoices):
+        GOAL = 'goal', 'Goal'
+        ASSIST = 'assist', 'Assist'
+        YELLOW_CARD = 'yellow card', 'Yellow Card'
+        RED_CARD = 'red card', 'Red Card'
+
+    action = models.CharField(max_length=50, choices=Actions.choices)
+
+    def __str__(self):
+        return f"{self.minutes}' - {self.player.name} - {self.action}"
